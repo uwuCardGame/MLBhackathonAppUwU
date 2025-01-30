ac240aa0415b8ffa36dbee0e2ee2efd5e8adef3a
Eunice <eunicetantws@gmail.com>
2025-01-31 04:12:52 +0800

    Fix uwu button

--- UwUCardGame/UwU.java
+++ UwUCardGame/UwU.java
@@ -3,10 +3,22 @@ import java.awt.Label;
 import javax.swing.JPanel;
 
 public class UwU extends JPanel{
+    private Display display;
+    private Header header;
     public UwU(){
-        this.setBackground(new Color(249, 224, 224));
-        this.add(new Label("uwu"));
-
-        System.out.println("you summoned uwu!!");
+        header = new Header();
+        this.add(header);
+        display = new Display();
+        this.add(display);
+    }
+    public class Header extends JPanel{
+        public Header(){
+            this.setBackground(new Color(165, 165, 165));
+        }
+    }
+    public class Display extends JPanel{
+        public Display(){
+            this.setBackground(new Color(0, 45, 114));
+        }
     }
 }
